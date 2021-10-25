9a670881bfc3c5ba8f7cca2dd0bd417df40ce3ba
Matis DENE <matis.dene@edu.devinci.fr>
2021-10-26 00:05:20 +0200

    :lipstick: css update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var span3 = document.getElementById("span3")
 var matis = document.getElementById("matis")
 var portfolio = document.getElementById("portfolio")
 var cross = document.getElementById("cross")
-
+var about = document.getElementById("about")
 
 burger.addEventListener('click',function(){
     span1.style.height = "100%"
@@ -16,6 +16,8 @@ burger.addEventListener('click',function(){
     matis.style.bottom = "800px"
     portfolio.style.bottom = "1000px"
     cross.style.visibility = "visible"
+    about.style.visibility = "visible"
+    about.style.transitionDelay = "0.4s"
 })
 cross.addEventListener('click',function(){
     span1.style.height = "0%"
@@ -25,5 +27,7 @@ cross.addEventListener('click',function(){
     matis.style.bottom = "60px"
     portfolio.style.bottom = "210px"
     cross.style.visibility = "hidden"
+    about.style.visibility = "hidden"
+    about.style.transitionDelay = "0s"
 })
 

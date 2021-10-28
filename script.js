cc6d5b9c1cb3124ea75c16c396cc84992a52a6e0
Matis DENE <matis.dene@edu.devinci.fr>
2021-10-28 14:26:57 +0200

    :lipstick: last css update homepage

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var pres = document.getElementById("pres")
 var span1 = document.getElementById("span1")
 var span2 = document.getElementById("span2")
 var span3 = document.getElementById("span3")
-var matis = document.getElementById("matis")
 var portfolio = document.getElementById("portfolio")
 var cross = document.getElementById("cross")
 var about = document.getElementById("about")
@@ -13,19 +12,17 @@ burger.addEventListener('click',function(){
     span2.style.height = "100%"
     span3.style.height = "100%"
     burger.style.visibility = "hidden"
-    matis.style.bottom = "800px"
     portfolio.style.bottom = "1000px"
     cross.style.visibility = "visible"
     about.style.visibility = "visible"
-    about.style.transitionDelay = "0.4s"
+    about.style.transitionDelay = "1s"
 })
 cross.addEventListener('click',function(){
     span1.style.height = "0%"
     span2.style.height = "0%"
     span3.style.height = "0%"
     burger.style.visibility = "visible"
-    matis.style.bottom = "60px"
-    portfolio.style.bottom = "210px"
+    portfolio.style.bottom = "230px"
     cross.style.visibility = "hidden"
     about.style.visibility = "hidden"
     about.style.transitionDelay = "0s"

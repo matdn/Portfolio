727dd9d2ba97a7221f3e41402aeb506288710910
matisdn <matis.dene@edu.devinci.fr>
2022-01-31 12:13:40 +0100

    :lipstick: responsive css update

--- projects.js
+++ projects.js
@@ -10,33 +10,33 @@ var images = document.querySelectorAll("img")
 
 li1.addEventListener('mouseover', function(){
     img1.style.opacity = "100%"
-    console.log("ayo")
+    
 })
 li1.addEventListener('mouseout', function(){
     img1.style.opacity = "0%"
-    console.log("ayo")
+    
 })
 li2.addEventListener('mouseover', function(){
     img2.style.opacity = "100%"
-    console.log("ayo")
+    
 })
 li2.addEventListener('mouseout', function(){
     img2.style.opacity = "0%"
-    console.log("ayo")
+    
 })
 li3.addEventListener('mouseover', function(){
     img3.style.opacity = "100%"
-    console.log("ayo")
+    
 })
 li3.addEventListener('mouseout', function(){
     img3.style.opacity = "0%"
-    console.log("ayo")
+    
 })
 li4.addEventListener('mouseover', function(){
     img4.style.opacity = "100%"
-    console.log("ayo")
+    
 })
 li4.addEventListener('mouseout', function(){
     img4.style.opacity = "0%"
-    console.log("ayo")
+    
 })
